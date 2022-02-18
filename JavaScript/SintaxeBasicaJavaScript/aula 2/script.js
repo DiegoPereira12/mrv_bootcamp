8a4637e2484763525a21a2bc3bc4240df6e0af84
Diego Junior Pereira <diego.pereira@softplan.com.br>
2022-02-18 17:58:44 -0300

    atividades práticas

---
+++ JavaScript/SintaxeBasicaJavaScript/aula 2/script.js
@@ -0,0 +1,34 @@
+// tipos primitivos
+
+// boleanos 
+/*var vOuF =false
+console.log(typeof(vOuF))
+
+//number
+var numeroQualquer = 1;
+console.log(typeof(numeroQualquer))
+
+//string
+var nome = 'diego';
+console.log(typeof(nome))*/
+
+//como declarar
+/*var variavel = 'diego'
+variavel = 'Pereira'
+console.log(variavel)*/
+
+let variavel2 = 'junior'
+variavel2 = 'junior2'
+console.log(variavel2)
+
+const constante = 'pedro';
+console.log(constante)
+
+var escopoGlobal = 'global';
+console.log(escopoGlobal);
+
+function escopoLocal(){
+    let escopoLocalInterno = 'local';    
+    console.log(escopoLocalInterno)
+}
+escopoLocal()
