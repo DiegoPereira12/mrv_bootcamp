5dbcd72de71baba3816fed87550f8b4074184e35
unknown <diegoo.junior55@gmail.com>
2022-02-20 00:12:32 -0300

    Atividades praticas

--- JavaScript/SintaxeBasicaJavaScript/aula 4 - Estrutura condicionais/script.js
+++ JavaScript/SintaxeBasicaJavaScript/aula 4 - Estrutura condicionais/script.js
@@ -32,3 +32,40 @@ switch (placar){
             default:
                 console.log('Ninguém ganhou')
 }
+
+// for
+let array = ['valor1', 'valor2','valor3', 'valor4', 'valor5',];
+
+let object = {propriedade1:'valor1', propriedade2: 'valoe2', propriedade3:'valor3'}
+
+for(let indice = 0; indice <array.length; indice++){
+    console.log(indice)
+}
+
+// for/in executa uma repetição a partir de uma propriadade
+for(i in array){
+    console.log(i)
+}
+
+// com object
+for (i in object){
+    console.log(i)
+}
+
+// for/of
+for (i of array){
+    console.log(i);
+}
+
+// for/of com object
+for(i of object.propriedade1){
+    console.log(i);
+}
+
+//while
+var a = 0;
+
+while (a < 10){
+    a++;
+    console.log(a);
+}
