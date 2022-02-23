2cf5da754dac226a60bd2327194f73371e3128bd
unknown <diegoo.junior55@gmail.com>
2022-02-22 22:34:05 -0300

    atividades práticas

---
+++ JavaScript/SintaxeBasicaJavaScript/aula 6 - apronfundando em funções/calculadora.js
@@ -0,0 +1,6 @@
+function calculadora(){
+    const operacao = prompt('Escolha uma operação:\n 1 - Soma (+)\n 2 - Subtração (-)\n 3 - Multiplicação (*)\n- 4 - Divisão real (/)\n 4 - Divisão inteira (%)\n 6 - Potenciação (**)');
+
+    console.log(operacao)
+}
+calculadora();
---
+++ JavaScript/SintaxeBasicaJavaScript/aula 6 - apronfundando em funções/script.js
@@ -0,0 +1,24 @@
+//// tipos de função
+
+// declarativa
+function funcao(){
+    console.log('Sou uma mensagem de uma funçaõ declarativa')
+}
+
+funcao();
+
+/// expressão de função
+// com nomeação obs, é opcional
+
+var funcao1 = function funcao1(){
+    console.log('Sou uma mensagem de função de expressão')
+}
+
+funcao1();
+
+//arrow function
+var funcao2 = () => {
+    console.log('Sou uma arrow function')
+}
+
+funcao2();
